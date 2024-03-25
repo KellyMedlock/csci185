68ece3d7ab5e75db001a3c9581006e8cb63b37ba
Kelly <ekmedlock2@gmail.com>
2024-03-25 13:07:56 -0400

    project1b update/new lecture file

---
+++ lectures/lecture16/02-function-with-parameters/main.js
@@ -0,0 +1,15 @@
+// function changeRed() {
+//     document.querySelector("body").style.backgroundColor = "red";
+// }
+
+// function changeBlue() {
+//     document.querySelector("body").style.backgroundColor = "blue";
+// }
+
+// function changeGreen() {
+//     document.querySelector("body").style.backgroundColor = "green";
+// }
+
+function changeColor(color) {
+    document.querySelector("body").style.backgroundColor = color
+}
