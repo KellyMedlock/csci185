f0c97df06705ecb6d29b8504c6893cec5e3c98fd
Kelly <ekmedlock2@gmail.com>
2024-04-20 13:31:45 -0400

    added tutorial/project02

---
+++ projects/project02b/main.js
@@ -0,0 +1,6 @@
+let map = L.map('map').setView([35.5951, -82.5515], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
