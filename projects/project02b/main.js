db6d4840ee5a65398e97f21a363a7c5ca0baa475
Kelly <ekmedlock2@gmail.com>
2024-04-21 09:42:02 -0400

    project02b update

--- projects/project02b/main.js
+++ projects/project02b/main.js
@@ -1,4 +1,7 @@
 let map = L.map('map').setView([35.5951, -82.5515], 13);
+let markerDowntown = L.marker([35.59795, -82.55577]).addTo(map);
+
+markerDowntown.bindPopup("<span class='popup'><h2><b>Chai Pani</b></h2><br><p>This is one of the best resturants in town. Be sure to try the okra fries!</p><br><img src='images/chaipani.jpg' class='chaipani'></span>").openPopup();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
